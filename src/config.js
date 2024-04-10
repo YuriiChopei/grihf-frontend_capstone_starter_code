602be0f5d8cb4beb726934d1cb9a395f6e02f920
Yurii Chopei <yura.chopey.97@gmail.com>
2024-04-10 18:21:23 -0400

    initial commit message

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const API_URL = window.location.hostname === "localhost" ? "https://yurachopey97-5500.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "https://yurachopey97-5500.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/";
+console.log(
+    "API_URL :",
+    API_URL
+);
