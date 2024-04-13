bbcd41f75d19a645287747224a95101b86e8461c
YuriiChopei <165403787+YuriiChopei@users.noreply.github.com>
2024-04-13 12:57:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/Landing_Page/Landing_page'};
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
-import Appointments from './Components/Appointments/Appointments';
-import SignUp from './Components/SignUp/SignUp';
+import LandingPage from './Components/Landing_Page/LandingPage';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing_Page/>}/>
-          <Route path="/appointments" element={<Appointments />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
 
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Notification>
+                    <Navbar />
+                    <Routes>
+                        <Route path="/LandingPage" element={<LandingPage />} />
+                        <Route path="/Sign_Up" element={<Sign_Up />} />
+                        <Route path="/Login" element={<Login />} />
+                      
+                </Notification>
+            </BrowserRouter>
+        </div>
+    );
+}
 export default App;
